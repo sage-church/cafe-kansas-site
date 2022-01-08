d9ea4f9f038d7ad288987249444a7517cd8dacd4
sage-church <sagin.churchman77@gmail.com>
2022-01-07 21:10:32 -0600

    up to beginning to create thank you pop-up

--- cafe_order.js
+++ cafe_order.js
@@ -48,13 +48,13 @@ $(".quantity-input").change(function () {
 
     for (quantElem of quantElemArray) {
 
-        var elemValue = quantElem.value,
+        var quantValue = quantElem.value,
             matchExpression = /\d+\.*\d+/g,
             itemPrice = $(quantElem).prev().html().match(matchExpression),
-            totalItemDollars = elemValue * itemPrice;
+            totalItemDollars = quantValue * itemPrice;
 
         totalDollarAmount += totalItemDollars;
-        document.getElementById("total-dollar-amount").innerHTML =
+        document.getElementById("total-dollar-amount").value =
             "$" + totalDollarAmount.toFixed(2);
     }
     if (totalDollarAmount != 0) {
@@ -64,21 +64,94 @@ $(".quantity-input").change(function () {
     };
 });
 
-//pop-up window functionality
+//variables for pop-up window functionality
 var checkoutButton = document.getElementById("checkout-button"),
     checkoutPopUp = document.getElementById("checkout-pop-up"),
+    grandTotalDiv = document.getElementById("grand-total-div"),
     cancel = document.getElementById("cancel"),
     greyOut = document.getElementById("grey-out"),
-    bodyObject = document.body;
+    bodyObject = document.body,
+    submitButton = document.getElementById("submit"),
+    firstNameInputBox = document.getElementById("first-name"),
+    lastNameInputBox = document.getElementById("last-name"),
+    phoneInputBox = document.getElementById("phone"),
+    phoneExp = /^\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ]([0-9]{4})$/;
 
+//display checkout pop-up window and disable rest of page
 checkoutButton.onclick = function () {
+    var grandTotal = document.getElementById("total-dollar-amount").value;
+
     checkoutPopUp.style.display = "block";
+    grandTotalDiv.innerHTML = grandTotal;
     greyOut.style.display = "block";
     bodyObject.classList.add("only-pop-up")
 };
 
+//functions to revert input box styles
+function revertFirstNameStyle () {
+    firstNameInputBox.style.border = "rgb(104 104 104) solid 1px"
+    firstNameInputBox.setAttribute("placeholder", "")
+}
+function revertLastNameStyle () {
+    lastNameInputBox.style.border = "rgb(104 104 104) solid 1px"
+    lastNameInputBox.setAttribute("placeholder", "")
+}
+function revertPhoneNumberStyle () {
+    phoneInputBox.style.border = "rgb(104 104 104) solid 1px"
+}
+
+
+//cancel checkout pop-up
 cancel.onclick = function () {
     checkoutPopUp.style.display = "none";
     greyOut.style.display = "none";
-    bodyObject.classList.remove("only-pop-up")
+    bodyObject.classList.remove("only-pop-up");
+    revertFirstNameStyle();
+    revertLastNameStyle();
+    revertPhoneNumberStyle();
+};
+
+//revert input box styles on input change (if input is valid)
+firstNameInputBox.addEventListener("change", function () {
+    if (this.value != "") {
+        this.style.border = "rgb(104 104 104) solid 1px"
+        this.setAttribute("placeholder", "")
+    }
+});
+
+lastNameInputBox.addEventListener("change", function () {
+    if (this.value != "") {
+        this.style.border = "rgb(104 104 104) solid 1px"
+        this.setAttribute("placeholder", "")
+    }
+});
+
+phoneInputBox.addEventListener("change", function () {
+    if (phoneExp.test(phoneInputBox.value) == true) {
+        phoneInputBox.style.border = "rgb(104 104 104) solid 1px"
+    }
+});
+
+//verify inputs before submission
+submitButton.onclick = function () {
+
+    if (firstNameInputBox.value == "") {
+        firstNameInputBox.style.border = "red solid 2px"
+        firstNameInputBox.setAttribute("placeholder", "Please enter first name")
+    };
+
+    if (lastNameInputBox.value == "") {
+        lastNameInputBox.style.border = "red solid 2px"
+        lastNameInputBox.setAttribute("placeholder", "Please enter last name")
+    };
+
+    if (phoneExp.test(phoneInputBox.value) == false) {
+        phoneInputBox.style.border = "red solid 2px"
+    };
+
+    if (firstNameInputBox.value != "" &&
+        lastNameInputBox.value != "" &&
+        phoneExp.test(phoneInputBox.value) == true) {
+            
+        }
 };
