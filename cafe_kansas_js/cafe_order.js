507159cfb1e79805354568c1755381dc668827c0
sage-church <sagin.churchman77@gmail.com>
2022-03-06 13:12:43 -0600

    Fixed bug where order total wouldn't reset to bash after completing checkout screen and exiting thank you pop up

--- cafe_kansas_js/cafe_order.js
+++ cafe_kansas_js/cafe_order.js
@@ -1,5 +1,7 @@
-//handle toggle of menu categories on Place Order page
+// All code is for functionality on 'Place Order' page
 
+// Once the +/- button is clicked, the menu category toggles (is hidden or shown).
+// If the button is currently a '+' it changes to '-', and vice versa.
 $(".tile-show-or-hide").click(function () {
     if (
         $(this).parent().next().css("display") == "none" ||
@@ -15,56 +17,68 @@ $(".tile-show-or-hide").click(function () {
     $(this).parent().next().slideToggle("fast")
 });
 
-
-//validate quantity inputs
-
+// When a user provides input through keyboard or copy/paste in the item quantity fields, 
+// the input is verified to be a number within the range 0 - 15.
 $(".quantity-input").change(function () {
 
     var quantity = parseInt($(this).val()),
         minimum = parseInt($(this).attr("min")),
         maximum = parseInt($(this).attr("max"));
 
+    // If entry is blank, set value to 0
     if (isNaN(quantity)) {
         $(this).val(0)
 
+    // If entry is smaller than min, set value to 0
     } else if (quantity < minimum) {
         $(this).val(minimum)
 
+    // If entry is greater than max, set value to 15
     } else if (quantity > maximum) {
         $(this).val(maximum)
 
+    // If entry is within range, set value to entry (essentially rounding down if applicable)
     } else {
         $(this).val(parseInt(quantity, 10))
     }
 });
 
-//update total dollar amount
 
+// Box where order total is displayed
+var orderTotalDisplayed = document.getElementById("total-dollar-amount");
+
+// Every time an entry in any item quantity field is made, the 'Order Total' at bottom of 
+// page is updated according to the item prices.
 $(".quantity-input").change(function () {
 
-    var quantElemArray = document.querySelectorAll(".quantity-input"),
+        // Access all quantity fields to use values
+    var quantElemList = document.querySelectorAll(".quantity-input"),
+        // Access div for checkout button in order to display/hide
         checkoutButtonBox = document.getElementById("checkout-button-box"),
-        totalDollarAmount = 0;
+        orderTotal = 0;
 
-    for (quantElem of quantElemArray) {
+    // Calculate and update order total
+    for (quantElem of quantElemList) {
 
-        var quantValue = quantElem.value,
+        var quant = quantElem.value,
             matchExpression = /\d+\.*\d+/g,
             itemPrice = $(quantElem).prev().html().match(matchExpression),
-            totalItemDollars = quantValue * itemPrice;
+            totalItemDollars = quant * itemPrice;
 
-        totalDollarAmount += totalItemDollars;
-        document.getElementById("total-dollar-amount").value =
-            "$" + totalDollarAmount.toFixed(2);
+        orderTotal += totalItemDollars;
+        orderTotalDisplayed.value =
+            "$" + orderTotal.toFixed(2);
     }
-    if (totalDollarAmount != 0) {
+    // The checkout button will only be displayed if the order total is not 0 to prevent
+    // a user from checking out with no items selected.
+    if (orderTotal != 0) {
         checkoutButtonBox.style.display = "flex"
     } else {
         checkoutButtonBox.style.display = "none"
     };
 });
 
-//variables for functionality of pop-up windows
+// Variables for functionality of pop-up windows
 var checkoutButton = document.getElementById("checkout-button"),
     checkoutPopUp = document.getElementById("checkout-pop-up"),
     popUptotal = document.getElementById("pop-up-total"),
@@ -76,61 +90,34 @@ var checkoutButton = document.getElementById("checkout-button"),
     lastNameInputBox = document.getElementById("last-name"),
     phoneInputBox = document.getElementById("phone"),
     thankYouPopUp = document.getElementById("thank-you-pop-up"),
-    exit = document.getElementById("exit")
-phoneExp = /^\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ]([0-9]{4})$/;
+    exit = document.getElementById("exit"),
+    phoneExp = /^\(?([0-9]{3})\)?[-. ]([0-9]{3})[-. ]([0-9]{4})$/;
 
-//display checkout pop-up window and disable rest of page
+// Display checkout pop-up window and disable rest of page
 checkoutButton.onclick = function () {
     var finalTotal = document.getElementById("total-dollar-amount").value;
-
     checkoutPopUp.style.display = "flex";
     popUptotal.innerHTML = finalTotal;
     greyOut.style.display = "block";
+    // Class attribute is added to the body to activate a CSS selector in the 'order.css' 
+    // file. The selector disables all click events for most elements on page (the checkout
+    // pop up is not affected due to separate id selector enabling click events on this one
+    // element).
     bodyObject.classList.add("only-pop-up")
 };
 
-//function to change input box styles indicating invalid input
-function invalidInputFormat() {
-    firstNameInputBox.style.border = "red solid 2px"
-    firstNameInputBox.setAttribute("placeholder", "Required")
-};
-
-//functions to revert input box styles
-
-function revertFirstNameStyle() {
-    firstNameInputBox.style.border = "rgb(104 104 104) solid 1px"
-    firstNameInputBox.setAttribute("placeholder", "")
-}
-function revertLastNameStyle() {
-    lastNameInputBox.style.border = "rgb(104 104 104) solid 1px"
-    lastNameInputBox.setAttribute("placeholder", "")
-}
-function revertPhoneNumberStyle() {
-    phoneInputBox.style.border = "rgb(104 104 104) solid 1px"
-}
-
-
-//cancel checkout pop-up
-//revert functions would only be applicable if styles have been changed due to...
-//      user submitting invalid entries
-cancel.onclick = function () {
-    checkoutPopUp.style.display = "none";
-    greyOut.style.display = "none";
-    bodyObject.classList.remove("only-pop-up");
-    revertFirstNameStyle();
-    revertLastNameStyle();
-    revertPhoneNumberStyle();
-};
-
-//verify inputs before submission
+// Verifies inputs upon submission. If inputs are invalid, the formatting is changed to 
+// indicate so, and the checkout process is not completed.
 submitButton.onclick = function () {
 
     if (firstNameInputBox.value == "") {
-        invalidInputFormat();
+        firstNameInputBox.style.border = "red solid 2px"
+        firstNameInputBox.setAttribute("placeholder", "Required")
     };
 
     if (lastNameInputBox.value == "") {
-        invalidInputFormat();
+        lastNameInputBox.style.border = "red solid 2px"
+        lastNameInputBox.setAttribute("placeholder", "Required")
     };
 
     if (phoneExp.test(phoneInputBox.value) == false) {
@@ -145,28 +132,54 @@ submitButton.onclick = function () {
     }
 };
 
-//revert input box styles on input change (if input is valid)
+// These are to revert input box formatting
+
+function revertFieldFormat(formField) {
+    formField.style.border = "rgb(104 104 104) solid 1px"
+    formField.setAttribute("placeholder", "")
+}
+
+function revertPhoneNumberStyle() {
+    phoneInputBox.style.border = "rgb(104 104 104) solid 1px"
+}
+
+// Input box formatting is reverted if valid input is received (would only be applicable
+// if invalid input was previously received).
 firstNameInputBox.addEventListener("change", function () {
     if (this.value != "") {
-        revertFirstNameStyle();
+        revertFieldFormat(this);
     }
 });
 
 lastNameInputBox.addEventListener("change", function () {
     if (this.value != "") {
-        revertLastNameStyle();
+        revertFieldFormat(this);
     }
 });
 
 phoneInputBox.addEventListener("change", function () {
-    if (phoneExp.test(phoneInputBox.value) == true) {
+    if (phoneExp.test(phoneInputBox.value) === true) {
         revertPhoneNumberStyle();
     }
 });
 
-//exit thank you pop-up
+// If user clicks the 'Cancel' button, the checkout pop up will be closed/hidden. When this
+// occurs, the input field formatting is reverted which is only applicable if user had
+// previously entered invalid input. 
+cancel.onclick = function () {
+    checkoutPopUp.style.display = "none";
+    greyOut.style.display = "none";
+    bodyObject.classList.remove("only-pop-up");
+    revertFieldFormat(firstNameInputBox);
+    revertFieldFormat(lastNameInputBox);
+    revertPhoneNumberStyle();
+};
+
+// Exits thank you pop up that is displayed after checkout is complete. 
+// TODO: Reset order total after this occurs
 exit.onclick = function () {
     thankYouPopUp.style.display = "none"
     greyOut.style.display = "none";
     bodyObject.classList.remove("only-pop-up");
+    orderTotalDisplayed.value = "$0.00"
 }
